aff8d2791d58fa015cb8ba73fcf06e5f855d4bd7
zuzanaolajcova <zuzana.olajcova@h2o.ai>
2019-11-21 20:20:11 +0100

    PUBDEV-6754 test fix

--- h2o-r/tests/testdir_algos/sharedtree/runit_PUBDEV-6754_ntrees_actual_for_tree_algos.R
+++ h2o-r/tests/testdir_algos/sharedtree/runit_PUBDEV-6754_ntrees_actual_for_tree_algos.R
@@ -24,40 +24,39 @@ test.tree_algos.bernoulli <- function() {
   prostate_gbm.h2o <- h2o.gbm(x = 3:9, y = "CAPSULE", training_frame = prostate.hex, nfolds = 5, distribution = "bernoulli", ntrees = ntrees, stopping_metric="MSE", stopping_tolerance=0.01, stopping_rounds=5)
  
   Log.info("GBM Model: number of trees set by user before building the model is:"); print(ntrees)
-  Log.info("GBM Model: number of trees built with early-stopping is:"); print(h2o.get_ntrees_actual(prostate_gbm))
+  Log.info("GBM Model: number of trees built with early-stopping is:"); print(h2o.get_ntrees_actual(prostate_gbm.h2o))
 
-  expect_true(h2o.get_ntrees_actual(prostate_gbm) < ntrees)
-  expect_equal(h2o.get_ntrees_actual(prostate_gbm), prostate_gbm.h2o@model$model_summary['number_of_trees'][,1])
+  expect_true(h2o.get_ntrees_actual(prostate_gbm.h2o) < ntrees)
+  expect_equal(h2o.get_ntrees_actual(prostate_gbm.h2o), prostate_gbm.h2o@model$model_summary['number_of_trees'][,1])
 
   # Train H2O Isolation Forest Model:
-  Log.info(paste("H2O Isolation Forest with parameters:\nsample_rate = 0.1, max_depth = 20, ntrees = ", ntrees, ", stopping_metric=\"anomalyscore\", stopping_tolerance=0.01, stopping_rounds=5\n", sep = ""))
-  prostate_if.h2o <- h2o.isolationForest(sample_rate = 0.1, max_depth = 20, ntrees=ntrees, stopping_metric="anomalyscore", stopping_tolerance=0.01, stopping_rounds=5)
+  Log.info(paste("H2O Isolation Forest with parameters:\nsample_rate = 0.1, max_depth = 20, ntrees = ", ntrees, ", stopping_metric=\"AUTO\", stopping_tolerance=0.01, stopping_rounds=5\n", sep = ""))
+  prostate_if.h2o <- h2o.isolationForest(sample_rate = 0.1, max_depth = 20, training_frame = prostate.hex, ntrees=ntrees, stopping_metric="AUTO", stopping_tolerance=0.01, stopping_rounds=5)
 
   Log.info("Isolation Forest Model: number of trees set by user before building the model is:"); print(ntrees)
-  Log.info("Isolation Forest Model: number of trees built with early-stopping is:"); print(h2o.get_ntrees_actual(prostate_if))
+  Log.info("Isolation Forest Model: number of trees built with early-stopping is:"); print(h2o.get_ntrees_actual(prostate_if.h2o))
 
-  expect_true(h2o.get_ntrees_actual(prostate_if) < ntrees)
-  expect_equal(h2o.get_ntrees_actual(prostate_if), prostate_if.h2o@model$model_summary['number_of_trees'][,1]) 
+  expect_true(h2o.get_ntrees_actual(prostate_if.h2o) < ntrees)
+  expect_equal(h2o.get_ntrees_actual(prostate_if.h2o), prostate_if.h2o@model$model_summary['number_of_trees'][,1]) 
 
   # Train H2O Random Forest Model:
-  Log.info(paste("H2O Random Forest with parameters:\nmax_depth=20, min_rows=10, ntrees = ", ntrees, ", stopping_metric=\"auc\", stopping_tolerance=0.01, stopping_rounds=5\n", sep = ""))
-  prostate_rf.h2o <- h2o.randomForest(ntrees=ntrees, max_depth=20, min_rows=10, stopping_metric="auc", stopping_tolerance=0.01, stopping_rounds=5)
+  Log.info(paste("H2O Random Forest with parameters:\nx = 1:4, y = 5,max_depth=20, min_rows=10, ntrees = ", ntrees, ", stopping_metric=\"AUTO\", stopping_tolerance=0.01, stopping_rounds=5\n", sep = ""))
+  prostate_rf.h2o <- h2o.randomForest(x = 1:4, y = 5, ntrees=ntrees, max_depth=20, min_rows=10, training_frame = prostate.hex, stopping_metric="AUTO", stopping_tolerance=0.01, stopping_rounds=5)
 
   Log.info("Random Forest Model: number of trees set by user before building the model is:"); print(ntrees)
-  Log.info("Random Forest Model: number of trees built with early-stopping is:"); print(h2o.get_ntrees_actual(prostate_rf))
+  Log.info("Random Forest Model: number of trees built with early-stopping is:"); print(h2o.get_ntrees_actual(prostate_rf.h2o))
 
-  expect_true(h2o.get_ntrees_actual(prostate_rf) < ntrees)
-  expect_equal(h2o.get_ntrees_actual(prostate_rf), prostate_rf.h2o@model$model_summary['number_of_trees'][,1])
+  expect_true(h2o.get_ntrees_actual(prostate_rf.h2o) < ntrees)
+  expect_equal(h2o.get_ntrees_actual(prostate_rf.h2o), prostate_rf.h2o@model$model_summary['number_of_trees'][,1])
 
   # Train H2O XGBoost Model:
-  Log.info(paste("H2O XGBoost with parameters:\ndistribution=\"auto\", seed=1, ntrees = ", ntrees, ", stopping_metric=\"auc\", stopping_tolerance=0.01, stopping_rounds=5\n", sep = ""))
-  prostate_xgb <- h2o.xgboost(distribution="auto", ntrees=ntrees, seed=1, stopping_metric="auc", stopping_tolerance=0.01, stopping_rounds=5)
-
+  Log.info(paste("H2O XGBoost with parameters:\nx = 1:4, y = 5,distribution=\"auto\", seed=1, ntrees = ", ntrees, ", stopping_metric=\"AUTO\", stopping_tolerance=0.01, stopping_rounds=5\n", sep = ""))
+  prostate_xgb.h2o <- h2o.xgboost(x = 1:4, y = 5, distribution="AUTO",training_frame = prostate.hex, ntrees=ntrees, seed=1, stopping_metric="deviance", stopping_tolerance=0.01, stopping_rounds=1)
   Log.info("XGBoost Model: number of trees set by user before building the model is:"); print(ntrees)
-  Log.info("XGBoost Model: number of trees built with early-stopping is:"); print(h2o.get_ntrees_actual(prostate_xgb))
+  Log.info("XGBoost Model: number of trees built with early-stopping is:"); print(h2o.get_ntrees_actual(prostate_xgb.h2o))
 
-  expect_true(h2o.get_ntrees_actual(prostate_xgb) < ntrees)
-  expect_equal(h2o.get_ntrees_actual(prostate_xgb), prostate_xgb.h2o@model$model_summary['number_of_trees'][,1])  
+  expect_true(h2o.get_ntrees_actual(prostate_xgb.h2o) < ntrees)
+  expect_equal(h2o.get_ntrees_actual(prostate_xgb.h2o), prostate_xgb.h2o@model$model_summary['number_of_trees'][,1])  
 
 }
 
